324ab9830b2859835a0a5baeb621567c512b81fc
pratik2820 <130150715+pratik2820@users.noreply.github.com>
2023-06-09 14:24:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,12 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
   //   write your code here
+	for(key in sampleObject){
+		if(key=="red" || key=="green" || key=="white"){
+			return true;
+		}
+	}
+	return false;
 }
 
 // Do not change the code below
